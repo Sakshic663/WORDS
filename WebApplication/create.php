00eebfab908474451b6ed42f4e83948c41b60d00
Sakshic663 <40264238+Sakshic663@users.noreply.github.com>
2019-04-22 00:00:21 -0400

    updated HTML for create post

--- WebApplication/create.php
+++ WebApplication/create.php
@@ -43,60 +43,124 @@
 <!doctype html>
 <html lang="en">
 
+<!doctype html>
+
+<html lang="en">
+
+ 
+
 <head>
+
     <!-- important for compatibility charset -->
+
     <meta charset="utf-8">
+
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+
     <title>Create New Post</title>
+
     <link rel="stylesheet" type="text/css" href="styles.css">
 
+ 
+
+ 
 
     <meta name="author" content="">
+
     <meta name="keywords" content="">
+
     <meta name="description" content="">
+
 </head>
+
+ 
 
 <body class="inner-page">
 
+ 
+
     <header class="header-basic">
+
         <div class="header-limiter">
+
             <h1><a href="#">WORDS</a></h1>
+
             <nav>
+
                 <a href="logout.php" id="logout" class="button primary" title="SIGN OUT">SIGN OUT</a>
+
             </nav>
+
         </div>
+
     </header>
 
+ 
+
     <!--<div class="container">
+
                 <div class="header">
+
             <div class="row">
+
                 <div class="float-right">
-                    
+
+                   
+
                 <a href="logout.php" id="logout" class="button primary" title="SIGN OUT">SIGN OUT</a>
+
                 </div>
+
             </div>
+
         </div>-->
+
         <!--<div class="row">
-                    <div class="small-12 columns">
+
+                   <div class="small-12 columns">
+
                         <h1>New Post</h1>
+
                     </div>
+
         </div>-->
-    
+
+   
+
         <div class="err">
+
             <?php echo $error; ?>
+
         </div>
+
         <form method="post" enctype="multipart/form-data" id="newpost">
+
+            <img src="./createart.png" style="float: right;" id="createart">
+
             <h1>New Post</h1>
+
             <label>
+
                 Title<br />
+
                 <input maxlength="100" type="text" id="nameItem" name="itemName" value="" placeholder="Enter title" />
+
             </label><br /><br />
+
             <label>
+
                 Content<br />
+
                 <textarea name="itemDescription" placeholder="Enter content" id="contentItem" rows="20" cols="50" maxlength="200" style="overflow-y: scroll;"></textarea>
+
             </label> <br>
+
             <input type="submit" id='postCreate' value="Create Post" class="button primary" />
+
         </form>
 
+ 
+
 </body>
+
 </html>
