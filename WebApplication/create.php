6e448d27df3cd8efc43b35523eae74e7236403a4
Soha Parvez <31773939+parvez98@users.noreply.github.com>
2019-04-21 23:06:34 -0400

    header and dashboard updated

--- WebApplication/create.php
+++ WebApplication/create.php
@@ -22,10 +22,7 @@
             $error .= "A description is required.<br>";
         }else{
             $itemDescription = $_POST['itemDescription'];
-        }
-        
-        
-        
+        }   
     }
 ?>
 <!doctype html>
@@ -35,7 +32,9 @@
     <!-- important for compatibility charset -->
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title> New Post</title>
+    <title>Create New Post</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
 
     <meta name="author" content="">
     <meta name="keywords" content="">
@@ -43,38 +42,46 @@
 </head>
 
 <body class="inner-page">
-    <!-- MAIN Container Start here. -->
-    <div class="container">
-        <!-- Header Starts -->
-        <div class="header">
+
+    <header class="header-basic">
+        <div class="header-limiter">
+            <h1><a href="#">WORDS</a></h1>
+            <nav>
+                <a href="logout.php" id="logout" class="button primary" title="SIGN OUT">SIGN OUT</a>
+            </nav>
+        </div>
+    </header>
+
+    <!--<div class="container">
+                <div class="header">
             <div class="row">
                 <div class="float-right">
                     
                 <a href="logout.php" id="logout" class="button primary" title="SIGN OUT">SIGN OUT</a>
                 </div>
             </div>
-        </div>
-        <div class="row">
+        </div>-->
+        <!--<div class="row">
                     <div class="small-12 columns">
-                        <h1>New Listing</h1>
+                        <h1>New Post</h1>
                     </div>
-                    <!-- title /-->
-        </div>
+        </div>-->
     
         <div class="err">
             <?php echo $error; ?>
         </div>
-        <form method="post" enctype="multipart/form-data">
+        <form method="post" enctype="multipart/form-data" id="newpost">
+            <h1>New Post</h1>
             <label>
-                Item For Sale
-                <input maxlength="100" type="text" id="nameItem" name="itemName" value="" placeholder="Enter Item Name" />
-            </label>
+                Title<br />
+                <input maxlength="100" type="text" id="nameItem" name="itemName" value="" placeholder="Enter title" />
+            </label><br /><br />
             <label>
-                Description
-                <textarea name="itemDescription" placeholder="Brief Description" id="descriptionItem" rows="4" maxlength="200"></textarea>
+                Content<br />
+                <textarea name="itemDescription" placeholder="Enter content" id="contentItem" rows="20" cols="50" maxlength="200" style="overflow-y: scroll;"></textarea>
             </label> <br>
             <input type="submit" id='postCreate' value="Create Post" class="button primary" />
         </form>
-</body>
 
+</body>
 </html>
