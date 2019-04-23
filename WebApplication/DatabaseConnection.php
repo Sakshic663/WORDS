81a7780111b1b525abbee2df617ea96c03feadac
Sakshic663 <40264238+Sakshic663@users.noreply.github.com>
2019-04-23 02:56:51 -0400

    mysql

--- WebApplication/DatabaseConnection.php
+++ WebApplication/DatabaseConnection.php
@@ -4,11 +4,11 @@ class DatabaseConnection{
 
     private static $instance = null;
     private $connection;
-
-    private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'connect_me';
+    //mysql://b23b6b38e20261:52fc0339@us-cdbr-iron-east-02.cleardb.net/heroku_57e4adbd95c2fd5?reconnect=true
+    private $host = 'us-cdbr-iron-east-02.cleardb.net';
+    private $username = 'b23b6b38e20261';
+    private $password = '52fc0339';
+    private $database = 'heroku_57e4adbd95c2fd5';
 
     private function __construct(){
         $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
