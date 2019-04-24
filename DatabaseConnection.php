2b4b15233c8326cab23bc7b21e7c05e342818f1b
Sakshic663 <40264238+Sakshic663@users.noreply.github.com>
2019-04-24 10:32:50 -0400

    db

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -5,11 +5,11 @@ class DatabaseConnection{
     private static $instance = null;
     private $connection = null;
     
-    
-    private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'connect_me';
+    //mysql://b8a2f772c3256e:764d8db6@us-cdbr-iron-east-02.cleardb.net/heroku_ebead2fda490cab?reconnect=true
+    private $host = 'us-cdbr-iron-east-02.cleardb.net';
+    private $username = 'b8a2f772c3256e';
+    private $password = '764d8db6';
+    private $database = 'heroku_ebead2fda490cab';
 
     private function __construct(){
         $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);      
